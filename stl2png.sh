27e1c9b40fceb6667e332864c4c86109819a8f57
Johann Gail <johann.gail@mail.de>
2018-11-13 20:56:51 +0100

    For some unknown reason the rename doesn't work on my system. Let convert use directly the final name.

--- stl2png.sh
+++ stl2png.sh
@@ -3,5 +3,5 @@
 echo "import(\"$1\");" > /tmp/stl2png_$(basename $1).scad
 /usr/bin/openscad  --colorscheme="Tomorrow" --imgsize=$3,$3 -o $2.png /tmp/stl2png_$(basename $1).scad
 rm /tmp/stl2png_$(basename $1).scad
-/usr/bin/convert $2.png -fuzz 20% -transparent white $2.png
-mv $2.png $2
+/usr/bin/convert $2.png -fuzz 20% -transparent white png:$2
+rm $2.png $2
