c7c88103a95cfa595b465f063522e91dae5b60ba
Johann Gail <johann.gail@mail.de>
2018-11-13 21:13:54 +0100

    Change the colorscheme to get transparent background in both cases.

--- scad2png.sh
+++ scad2png.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-/usr/bin/openscad --colorscheme="Tomorrow Night" --imgsize=$3,$3 -o $2.png $1
+/usr/bin/openscad --colorscheme="Tomorrow" --imgsize=$3,$3 -o $2.png $1
 /usr/bin/convert $2.png -fuzz 20% -transparent white png:$2
 rm $2.png $2
--- stl2png.sh
+++ stl2png.sh
@@ -2,7 +2,7 @@
 
 SCAD=/tmp/stl2png_$(basename $2).scad
 echo "import(\"$1\");" > $SCAD
-/usr/bin/openscad  --colorscheme="Tomorrow" --imgsize=$3,$3 -o $2.png $SCAD
+/usr/bin/openscad  --colorscheme="Cornfield" --imgsize=$3,$3 -o $2.png $SCAD
 rm $SCAD
 /usr/bin/convert $2.png -fuzz 20% -transparent white png:$2
 rm $2.png $2
