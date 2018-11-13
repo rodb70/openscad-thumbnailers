7226c1a38705d57761725134a1d9d5bf45b36a5d
Johann Gail <johann.gail@mail.de>
2018-11-13 21:10:59 +0100

    The current solution didn't work, if the source contains blanks its name. Use the uniqe hashcode of the outfile as base.

--- stl2png.sh
+++ stl2png.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-echo "import(\"$1\");" > /tmp/stl2png_$(basename $1).scad
-/usr/bin/openscad  --colorscheme="Tomorrow" --imgsize=$3,$3 -o $2.png /tmp/stl2png_$(basename $1).scad
-rm /tmp/stl2png_$(basename $1).scad
+SCAD=/tmp/stl2png_$(basename $2).scad
+echo "import(\"$1\");" > $SCAD
+/usr/bin/openscad  --colorscheme="Tomorrow" --imgsize=$3,$3 -o $2.png $SCAD
+rm $SCAD
 /usr/bin/convert $2.png -fuzz 20% -transparent white png:$2
 rm $2.png $2
